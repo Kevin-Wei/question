3f821ee5279e95b3bfb187dae93108482a94adaa
Kevin Wei <kevin.tw.wei@gmail.com>
2013-05-07 22:48:11 -0700

    scaffold quick app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Questions::Application.config.secret_token = '20e5240dbc924e85c85e82cf334a8c779f5972269a490762e5cc4b0bf65892fa6f7eb7eb7f21ec8e5a2474e494ccd56bd64ea06d0343d88b2f6469c243dd2a43'
